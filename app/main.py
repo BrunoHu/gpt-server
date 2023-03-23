23f649cfcb419d3f8d0fe57490806b0c57aaa0e1
bates <hbc0204@foxmail.com>
2023-03-22 20:09:48 -0700

    first commit

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from typing import Union
+
+from fastapi import FastAPI
+from config_reader import settings
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+
+
+@app.get("/info")
+def info():
+    return {
+        "app_name": settings.app_name,
+        "admin_email": settings.admin_email,
+        "items_per_user": settings.items_per_user,
+    }
